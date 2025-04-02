adbd189e203c8dbb332819096ba43c45a9d35962
Amodbaghel <baghelamod012@gmail.com>
2025-04-02 14:16:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-26"
+    bucket = "oneplus-two-three"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
